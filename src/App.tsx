7261e2747e336acc80ffe36f58ddc4abbd8a2903
Glefferson Vicente <gleffersonv@gmail.com>
2022-09-16 17:08:03 -0300

    Semana NLW Projeto Esports

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { CreatAdBanner } from "./components/CreateAdBanner";
+import { GameBanner } from "./components/GameBanner";
+import axios from 'axios'
+
+import "./styles/main.css";
+
+import logoImg from "./assets/logo.svg";
+import { CreatAdModal } from "./components/CreatAdModal";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios("https://www.gleffersonvicente.com.br/esports/games/").then((response) => {
+      setGames(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
+      </div>
+      <Dialog.Root>
+        <CreatAdBanner />
+        <CreatAdModal />
+      </Dialog.Root>
+    </div>
+  );
+}
+export default App;
+
